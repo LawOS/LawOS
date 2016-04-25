45fd2076ebed8f7588992a3c70d5f3b1dc5af501
aviola02 <aviola02@cs.ucy.ac.cy>
2016-04-25 13:31:26 +0300

    check

--- LawOS/src/LawOS.java
+++ LawOS/src/LawOS.java
@@ -3,6 +3,7 @@ public class LawOS {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world! New commit");
+		System.out.println("Hello world! New commit");
 	}
 
 }
